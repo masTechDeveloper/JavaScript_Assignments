1be7b68599534f213bdf53db7edaed5497a2d519
masTechDeveloper <ashir342@gmail.com>
2019-10-10 01:58:07 +0500

    start JS Assignment 03.

---
+++ JS Assignment 03/script.js
@@ -0,0 +1,30 @@
+/* -------------- JavaScript Assignment 03 | Array -------------- */
+
+/*
+Q1. Declare and Initialize an array and store available education qualifications in
+    Pakistan(e.g.SSC, HSC, BCS, BS, BCOM, MS, M.Phil., PhD).Show the listed
+    qualifications in your browser like: 
+*/
+
+/* ---------- Solution ---------- */
+
+// var eduArr = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil.", "PhD"];
+
+// for (var a = 0; a < eduArr.length; a++) {
+//   document.write(a + " ) " + eduArr[a] + "<br>");
+// }
+
+/* Q2. Write a program to store 3 student names in an array.Take another array to store
+    score of these three students.Assume that total marks are 500 for each student,
+    display the scores & percentages of students like: */
+
+/* ---------- Solution ---------- */
+
+// var studentName = ["Shabir", "Arbaz", "Maaz"];
+
+// var marks = [460, 250, 350];
+
+// for (var i = 0; i < studentName.length; i++) {
+//   if () {
+//   }
+// }
