df32d9069855ee1ed76597d534e542c7d22c0631
masTech Developer <ashir342@gmail.com>
2019-10-24 02:09:07 +0500

    task02 complete

--- JS Assignment 04/task01/task01.js
+++ JS Assignment 04/task01/task01.js
@@ -1,1 +1,10 @@
-alert("Hello World");
+
+/* -----------------------  JavaScript Assignment 04 | Task 01 ----------------------- */
+
+
+/*  Q1. Write a program to replace the “Hyder” to “Islam” in the word
+        “Hyderabad” and display the result in your browser. */
+
+
+
+        
---
+++ JS Assignment 04/task02/task02.js
@@ -0,0 +1,19 @@
+
+/* -----------------------  JavaScript Assignment 04 | Task 02 ----------------------- */
+
+
+/*  Q2. Write a program that converts a string “472” to a number 472. Display
+        the values & types in your browser. */
+
+document.write("<b>Q2. Write a program that converts a string “472” to a number 472. Display the values & types in your browser.</b> <br> <br>");
+
+var stringValue = "472"; // Simple string value store in a variable.
+
+document.write("1st Value DataType is String : " + stringValue + "<br> <br>");
+
+
+var convertInt = parseInt(stringValue); // parseInt Method is convert String Value into Integer or Number.
+
+document.write("2nd Value DataType is Integer/Number : " + convertInt);
+
+
