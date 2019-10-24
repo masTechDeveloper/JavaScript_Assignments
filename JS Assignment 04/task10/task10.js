6bbf2f356098f2b53dcb67a4cd848493232a1428
masTech Developer <ashir342@gmail.com>
2019-10-25 01:02:35 +0500

    task10 complete

---
+++ JS Assignment 04/task10/task10.js
@@ -0,0 +1,31 @@
+/* -----------------------  JavaScript Assignment 04 | Task 09 ----------------------- */
+
+
+/*  10. Write a program that round a number to a given decimal places. */
+
+/* ------------ Soution with HTML UI ------------ */
+
+// function manageDecimals() {
+
+//     var entValue = parseFloat(document.getElementById("userInput").value),
+
+//         decPoint = parseInt(document.getElementById("decimalPlaces").value);
+
+//     var roundDec = entValue.toFixed(decPoint);
+
+//     document.getElementById("result").innerHTML = roundDec;
+
+// }
+
+
+/* ------------ Soution print console.log() ------------ */
+
+var input = parseFloat(prompt("Enter a Number. "));
+
+var dec = parseInt(prompt("Enter which decimal point you want !"));
+
+console.log("Value : " + input);
+
+var fix = input.toFixed(dec);
+
+console.log("Round Value : " + fix);
