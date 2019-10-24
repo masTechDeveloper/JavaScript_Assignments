d891caedb99cea2ed16b12b139717fa43efa7677
masTech Developer <ashir342@gmail.com>
2019-10-25 02:02:46 +0500

    task11 working

---
+++ JS Assignment 04/task11/task11.js
@@ -0,0 +1,23 @@
+/* -----------------------  JavaScript Assignment 04 | Task 11 ----------------------- */
+
+
+/*  Q11. Write a program to take input of a Number and find a Mean of All Digits. 
+    
+        E.g: 666 = 6 + 6 + 6 = 18 /3 = 6
+            1246 = 1 + 2 + 4 + 5 = 12 / 4 = 3 
+*/
+
+// var UserInput = [prompt("Enter Number")];
+
+
+
+// for (var a = 0; a < UserInput.length; a++) {
+//     console.log(UserInput);
+// }
+
+
+var val = 666;
+
+console.log(Math.abs(val % 1));
+console.log(Math.abs(val - 1));
+console.log(Math.abs(val + 1));
