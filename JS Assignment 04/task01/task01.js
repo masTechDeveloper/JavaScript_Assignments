bbdc48fb4737378538e6eaba9486a581030b59d1
masTech Developer <ashir342@gmail.com>
2019-10-25 21:31:28 +0500

    task01 modify

--- JS Assignment 04/task01/task01.js
+++ JS Assignment 04/task01/task01.js
@@ -1,10 +1,5 @@
-
 /* -----------------------  JavaScript Assignment 04 | Task 01 ----------------------- */
 
 
 /*  Q1. Write a program to replace the “Hyder” to “Islam” in the word
         “Hyderabad” and display the result in your browser. */
-
-
-
-        
