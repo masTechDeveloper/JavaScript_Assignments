ba0d311c498cb7d892a453edd2da94b6701951c0
masTech Developer <ashir342@gmail.com>
2019-10-25 21:40:08 +0500

    task01 complete

--- JS Assignment 04/task01/task01.js
+++ JS Assignment 04/task01/task01.js
@@ -3,3 +3,15 @@
 
 /*  Q1. Write a program to replace the “Hyder” to “Islam” in the word
         “Hyderabad” and display the result in your browser. */
+
+
+
+var actualWord = "Hyderabad";
+
+var replaceWord = actualWord.replace(/Hyder/g, "Islam");
+
+document.write("Actual Word is :- " +
+    actualWord +
+    "<br>" +
+    "Replace Word is :- " +
+    replaceWord);
