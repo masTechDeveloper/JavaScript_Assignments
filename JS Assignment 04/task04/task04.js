2b3af2c6ff03a44dc7129ba9f42b588e8ed70401
masTech Developer <ashir342@gmail.com>
2019-10-25 21:54:21 +0500

    task04 incomplete

---
+++ JS Assignment 04/task04/task04.js
@@ -0,0 +1,11 @@
+/* -----------------------  JavaScript Assignment 04 | Task 04 ----------------------- */
+
+
+/*  Q4. Write a program to validate email address with following evaluation
+            a. This string must contain @
+            b. Ths string must contact chars
+            c. The @ must have at least 1 char in front
+            d. The string must contain .(dot) after @ and at least 1 char. */
+
+
+var name;
