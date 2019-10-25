91d7646c516028427555d9b6a6752503c1f97cfa
masTech Developer <ashir342@gmail.com>
2019-10-25 21:41:06 +0500

    task11 complete

--- JS Assignment 04/task11/task11.js
+++ JS Assignment 04/task11/task11.js
@@ -6,18 +6,39 @@
         E.g: 666 = 6 + 6 + 6 = 18 /3 = 6
             1246 = 1 + 2 + 4 + 5 = 12 / 4 = 3 
 */
+var number = parseInt(prompt("Enter Some Value !")),
+    output = [],
+    sNumber = number.toString();
 
-// var UserInput = [prompt("Enter Number")];
+for (var i = 0, len = sNumber.length; i < len; i += 1) {
+    output.push(+sNumber.charAt(i));
+}
+
+for (var a = 0; a < sNumber.length; a++) {
+
+    var sum = output.reduce((a, b) => a + b, 0),
+        result = sum / sNumber.length;
+}
+
+
+console.log(output);
+
+console.log(sum);
+
+console.log(result);
 
 
 
-// for (var a = 0; a < UserInput.length; a++) {
-//     console.log(UserInput);
-// }
 
 
-var val = 666;
+/* Example
 
-console.log(Math.abs(val % 1));
-console.log(Math.abs(val - 1));
-console.log(Math.abs(val + 1));
+input 
+123
+    
+Output
+    
+123 = 1 + 2 + 3 = 6 / 3 = 2 
+    
+    
+*/
