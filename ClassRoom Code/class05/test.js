dcdee0cb9b9a1ba6dfbc8db142042425539b6f66
masTech Developer <ashir342@gmail.com>
2019-10-30 15:18:50 +0500

    modify task08.js

---
+++ ClassRoom Code/class05/test.js
@@ -0,0 +1,78 @@
+// function makeTea(sugar, milk) {
+
+//     if (milk) {
+//         console.log("Making Tea withh " + sugar + "Spoons with Milk");
+
+//     } else {
+//         console.log("Make tea with " + sugar)
+//     }
+
+// }
+
+
+// function getAvg(num1, num2, num3) {
+
+//     var avg1 = (num1 + num2 + num3) / 3;
+//     return avg1;
+// }
+
+// function showGrade(avg1) {
+//     if (avg > 80) {
+//         console.log("Grade A");
+//     } else if (avg > 70) {
+//         console.log("Grade B");
+//     } else if (avg > 60) {
+//         console.log("Grade C");
+//     } else if (avg > 50) {
+//         console.log("Grade D");
+//     } else {s
+//         console.log("Fail");
+//     }
+// }
+
+
+// function findGrade(sc01, sc02, sc03) {
+//     var avg = getAvg(sc01, sc02, sc03) / 3;
+//     console.log(avg);
+//     showGrade(avg);
+// }
+
+// findGrade(90, 60, 70);
+
+// var avg = getAvg(90, 80, 90);
+// showGrade(avg);
+// console.log(avg);
+
+
+
+
+// function calc(a, b, c) {
+
+//     if (c === "add") {
+//         var add = a + b;
+//         console.log(add);
+//         return add;
+//     } else if (c === "min") {
+//         var min = a - b;
+//         console.log(min);
+//     } else if (c === "mul") {
+//         var mul = a * b;
+//         console.log(mul);
+//     }
+
+// }
+// calc(90, 90, "add");
+
+
+
+
+
+
+
+
+
+
+
+// function throwdice() {
+//     return ((math.randon() * 6) + 1);
+// }
--- JS Assignment 04/task08/task08.js
+++ JS Assignment 04/task08/task08.js
@@ -1,3 +1,20 @@
+/* -----------------------  JavaScript Assignment 04 | Task 08 ----------------------- */
+
+
+/*  Q8. Convert number in words. Example
+            Input
+                23
+                123
+                1234
+            Output
+                Twenty Three
+                One Hundred and Twenty Three
+                One Thousand Two Hundred and Thirty Four. */
+
+
+/* -----------------------  Solution 01. ----------------------- */
+
+
 var a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
 var b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
@@ -17,3 +34,94 @@ function inWords(num) {
 document.getElementById('number').onkeyup = function() {
     document.getElementById('words').innerHTML = inWords(document.getElementById('number').value);
 };
+
+
+/* -----------------------  Solution 02. ----------------------- */
+
+
+
+var input = prompt("Enter any number From 1 to 10000");
+var unitMap = {
+    "0": '',
+    "1": 'one',
+    "2": 'two',
+    "3": 'three',
+    "4": 'four',
+    "5": 'five',
+    "6": 'six',
+    "7": 'seven',
+    "8": 'eight',
+    "9": 'nine',
+    "10": "ten",
+    "11": 'eleven',
+    "12": 'twelve',
+    "13": 'thirteen',
+    "14": 'fourteen',
+    "15": 'fifteen',
+    "16": 'sixteen',
+    "17": 'seventeen',
+    "18": 'eighteen',
+    "19": 'nineteen'
+};
+var tenUnit = {
+    "1": 'ten',
+    "2": 'twenty',
+    "3": 'thirty',
+    "4": 'fourty',
+    "5": 'fifty',
+    "6": 'sixty',
+    "7": 'seventy',
+    "8": 'eighty',
+    "9": 'ninety'
+};
+var hunUnit = {
+    "1": ' hundred '
+};
+var thousUnit = {
+    "1": ' thousand '
+};
+
+
+var answer;
+var length = input.length;
+
+
+if (parseInt(input) <= 19) {
+    answer = unitMap[input]
+
+} else if (input.length == 2) {
+    answer = tenUnit[input[0]] + " " + unitMap[input[1]];
+
+} else if (input.length == 3) {
+    answer = unitMap[input[0]] + hunUnit[1];
+
+    if (input.length == 3) {
+        answer = unitMap[input[0]] + hunUnit[1] + " & " + tenUnit[input[1]]
+    }
+    if (input.length == 3) {
+        answer = unitMap[input[0]] + hunUnit[1] + " & " + tenUnit[input[1]] + " " + unitMap[input[2]]
+    }
+} else if (input.length == 4) {
+    answer = unitMap[input[0]] + thousUnit[1]
+
+    if (input.length == 4) {
+        answer = unitMap[input[0]] + thousUnit[1] + "& " +
+            unitMap[input[1]] + hunUnit[1]
+    }
+    if (input.length == 4) {
+        answer = unitMap[input[0]] + thousUnit[1] + unitMap[input[1]] +
+            hunUnit[1] + "& " + tenUnit[input[2]]
+    }
+    if (input.length == 4) {
+        answer = unitMap[input[0]] + thousUnit[1] + unitMap[input[1]] +
+            hunUnit[1] + "& " + unitMap[input[3]]
+    }
+    if (input.length == 4) {
+        answer = unitMap[input[0]] + thousUnit[1] + unitMap[input[1]] +
+            hunUnit[1] + " & " + tenUnit[input[2]] + " " + unitMap[input[3]]
+    }
+} else if (input.length == 5) {
+    answer = tenUnit[input[0]] + thousUnit[1]
+}
+
+document.write("<br><br>Your input number is :- " + input + "<br>" + "Your integer inwords is :- " + answer);
