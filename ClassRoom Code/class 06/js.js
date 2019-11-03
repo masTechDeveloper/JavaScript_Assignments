d6842849c06b7c305d34407965624caea1763f65
masTech Developer <ashir342@gmail.com>
2019-11-03 16:22:45 +0500

    class 06 js file modify

--- ClassRoom Code/class 06/js.js
+++ ClassRoom Code/class 06/js.js
@@ -5,22 +5,44 @@
 
 // Random Number
 
-var arr = [];
+// var arr = [];
 
-for (var i = 0; i < 10; i++) {
-    var duplicateValue = false;
-    var rand = Math.floor(Math.random() * 10 + 1);
-    console.log("Index = " + i + " Random Number is = " + rand);
-    for (var n = 0; n < arr.length; n++) {
-        if (arr[n] == rand) {
-            duplicateValue = true;
-            i--;
-            break;
+// for (var i = 0; i < 10; i++) {
+//     var duplicateValue = false;
+//     var rand = Math.floor(Math.random() * 10 + 1);
+//     console.log("Index = " + i + " Random Number is = " + rand);
+//     for (var n = 0; n < arr.length; n++) {
+//         if (arr[n] == rand) {
+//             duplicateValue = true;
+//             i--;
+//             break;
+//         }
+//     }
+//     if (!duplicateValue) {
+//         arr[i] = rand;
+//     }
+// }
+
+// console.log(arr);
+
+
+
+// Finding a prime number 
+
+function test_prime(num) {
+
+    if (num === 1) {
+        return false;
+    } else if (num === 2) {
+        return true;
+    } else {
+        for (var x = 2; x < n; x++) {
+            if (n % x === 0) {
+                return false;
+            }
         }
-    }
-    if (!duplicateValue) {
-        arr[i] = rand;
+        return true;
     }
 }
 
-console.log(arr);
+console.log(test_prime(2));
