38f1972ea43abde31d3ef0c7b81ddf19caba3f52
masTech Developer <ashir342@gmail.com>
2019-11-12 00:56:55 +0500

    task09 complete

--- JS Assignment 05/task09/task09.js
+++ JS Assignment 05/task09/task09.js
@@ -1,40 +1,12 @@
 /* Q9. Write a function to add weeks in current date.  */
 
-var currentDate = new Date();
+var newWeek = parseInt(prompt("Add Weeks in current Date"));
 
-console.log(currentDate);
+function addWeeks(weeks) {
 
-var getWeeks = currentDate.getDate();
-
-console.log(getWeeks);
-
-var weeks = 2;
-
-var update = getWeeks + weeks;
-
-currentDate.setFullYear(update);
-
-
-
-console.log(currentDate);
-
-
-/*
-
-var currentDate = new Date();
-
-// Current Time
-document.write("Current Date & Time : " + currentDate + "<br><br>");
-
-var getCurrentHour = currentDate.getHours();
-
-var agoTime = parseInt(prompt("_____ Hour ago time ?"));
-
-var resetHour = getCurrentHour - agoTime;
-
-currentDate.setHours(resetHour);
-
-// Updated Hour Time is. 
-document.write(agoTime + " hour ago, it was " + currentDate);
-
-*/
+    let now = new Date();
+    now.setDate(now.getDate() + weeks * 7);
+    alert(now); // Shown in Alert message!
+    console.log(now); // Shown in console area.
+}
+addWeeks(newWeek);
