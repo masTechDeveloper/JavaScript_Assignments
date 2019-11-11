58fe6bb3de97f275f43522a021df8fa63cac089e
masTech Developer <ashir342@gmail.com>
2019-11-12 00:47:25 +0500

    task12 complete

--- JS Assignment 05/task12/task12.js
+++ JS Assignment 05/task12/task12.js
@@ -18,6 +18,8 @@
         Note: You must use while or do while loop and functions for this game
 */
 
+/* ------------- Solution 01 for loop ------------- */
+
 
 function guessNumber() {
 
@@ -54,3 +56,56 @@ function playAgain() {
         playAgain();
     }
 }
+
+/* ------------- Solution 02 DoW While Loop ------------- */
+
+// //function to check user guess
+// function checkUserGuess(user_guess) {
+//     switch (user_guess) {
+//         case random_num:
+//             alert("You are right, You won");
+//             break;
+//         default:
+//             alert("Incorrect, Please try again");
+//     }
+// }
+
+
+
+// // Ask user to guess the number
+
+// function userGuess() {
+//     var i = 0;
+//     while (i < 4) {
+//         switch (i) {
+//             case 0:
+//                 var user_guess = parseInt(prompt("Please guess a number between 1 to 100: "));
+//                 checkUserGuess(user_guess);
+
+//             case 1:
+//                 var user_guess = parseInt(prompt("Please guess a number between 1 to 100: "));
+//                 checkUserGuess(user_guess);
+
+//             case 2:
+//                 var user_guess = parseInt(prompt("Please guess a number between 1 to 100: "));
+//                 checkUserGuess(user_guess);
+
+//             default:
+//                 alert("You lose!");
+//                 var ask_again = prompt("Do you wish to play again? Yes/No");
+//         }
+//         i++;
+//         if (ask_again.toLowerCase() === "yes") {
+//             i = 0;
+//         } else if (ask_again.toLowerCase() === "no") {
+//             alert("Thank you for playing with us today.");
+//         }
+//     }
+// }
+
+
+// // generate a random number & call userGuess function
+// var random_num = Math.random();
+// random_num = (random_num * 100) + 1;
+// random_num = Math.floor(random_num);
+// userGuess();
